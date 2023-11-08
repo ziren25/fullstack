fae6be585adf16e7008d2e73c0f94287de74732f
Iwo Chodyniecki <chodyniecki12@gmail.com>
2023-11-08 13:14:23 +0200

    dasdas

---
+++ part1/src/App.jsx
@@ -0,0 +1,13 @@
+const Hello = () => {  return (    <div>      <p>Hello world</p>    </div>  )}
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello />    
+      <Hello />    
+      <Hello />    
+      </div>
+  )
+}
+
+export default App
